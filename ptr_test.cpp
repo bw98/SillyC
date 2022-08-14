04ef8b6dd38f59123fad4225488489afdfa1607d
bw98 <786016746@qq.com>
2022-08-14 14:53:49 -0400

    Check git is workable in new workplace

--- ptr_test.cpp
+++ ptr_test.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <memory>
 
-// std::shared_ptr<int> sp;
-std::shared_ptr<int> sp = std::make_shared<int>(666);
-std::weak_ptr<int> wp(sp);
-std::weak_ptr<int> wp222;
+// std::shared_ptr<int> SP;
+std::shared_ptr<int> SPTR = std::make_shared<int>(666);
+std::weak_ptr<int> WPTR(SPTR);
+std::weak_ptr<int> WPTR2;
 
 std::shared_ptr<int> get() {
-    std::shared_ptr<int> t = wp.lock();
+    std::shared_ptr<int> t = WPTR.lock();
     return t;
 }
 
@@ -17,17 +17,16 @@ int main () {
     std::cout << a.use_count() << std::endl;
     // std::shared_ptr<int> a = get();
     // std::cout << a.use_count() << std::endl;
-    
-
     if (a) {
         std::cout << "Not nullptr" << std::endl;
     } else {
         std::cout << "Nullptr" << std::endl;
     }
 
-    wp222 = sp;
-    if (wp222.lock()) {
+    WPTR2 = SPTR;
+    if (WPTR2.lock()) {
         std::cout << "Not nullptr" << std::endl;
+        std::cout << WPTR2.use_count() << std::endl;
     } else {
         std::cout << "Nullptr" << std::endl;
     }
