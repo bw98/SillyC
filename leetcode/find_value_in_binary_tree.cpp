8460ed3ea9dcbf957542467e752d8416f4436776
bw98 <786016746@qq.com>
2022-09-15 00:00:16 -0400

    Tree stuff

---
+++ leetcode/find_value_in_binary_tree.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <list>
+#include <forward_list>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <memory>
+#include <functional>
+#include <utility>
+#include <random>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <atomic>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+using std::array;
+using std::list;
+using std::forward_list;
+using std::map;
+using std::set;
+using std::pair;
+using std::make_pair;
+using std::unordered_map;
+using std::unordered_set;
+using std::multimap;
+using std::multiset;
+using std::shared_ptr;
+using std::weak_ptr;
+using std::unique_ptr;
+using std::make_shared;
+using std::make_unique;
+using std::allocator;
+using std::stringstream;
+using std::thread;
+using std::mutex;
+using std::shared_mutex;
+using std::lock_guard;
+using std::unique_lock;
+using std::shared_lock;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 输入一棵没有重复元素的二叉树根节点root和一个目标值val，请你写一个函数寻找树中值为val的节点
+    TreeNode* find(TreeNode* root, int val) {
+        if (!root) return nullptr;
+
+        if (root->val == val) {
+            return root;
+        } else {
+            TreeNode* t = find(root->left, val);
+            if (t) return t;
+            t = find(root->right, val);
+            if (t) return t;
+        }
+
+        return nullptr;
+    }
+};
+
