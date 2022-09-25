0a684d5ca31086aad4129b861e8c4527c3965e5c
bw98 <786016746@qq.com>
2022-09-25 00:13:51 -0400

    Count number of nodes in binary tree

---
+++ leetcode/count_tree_node.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <list>
+#include <forward_list>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <memory>
+#include <functional>
+#include <utility>
+#include <random>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <atomic>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+using std::array;
+using std::list;
+using std::forward_list;
+using std::map;
+using std::set;
+using std::pair;
+using std::make_pair;
+using std::unordered_map;
+using std::unordered_set;
+using std::multimap;
+using std::multiset;
+using std::shared_ptr;
+using std::weak_ptr;
+using std::unique_ptr;
+using std::make_shared;
+using std::make_unique;
+using std::allocator;
+using std::stringstream;
+using std::thread;
+using std::mutex;
+using std::shared_mutex;
+using std::lock_guard;
+using std::unique_lock;
+using std::shared_lock;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+count(TreeNode* t) {
+    if (!t) return 0;
+
+    return 1 + count(t->left) + count(t->right);
+}
+
